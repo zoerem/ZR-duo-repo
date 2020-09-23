999cc1c9b35a311f6ec93b838d3d43e7ec63da2c
zoerem <63026112+zoerem@users.noreply.github.com>
2020-09-23 13:35:58 -0700

    arrow anime

---
+++ mysite/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+// NOT WORKING --- need it to disappear and then come in from lef
+// either upon scroll or mouse enter, but scroll would be better.
+// $(#pC-1).on("mouseenter"{
+//     $("#hc-1").css("right" "-120%").css("right" "-40%");
+// })
+
+// $(".arrows-container").on(".mouseenter"{
+//     $(".arrow").slideToggle("slow");
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// below is closing for document-ready function
+});
